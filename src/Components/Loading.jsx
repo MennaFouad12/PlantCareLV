03633d5ba52eb682626a06d0e19c2a0b08c9b40c
MennaFouad12 <mm6331329@gmail.com>
2025-08-02 13:35:19 +0300

    Deploy updated2 build

--- src/Components/Loading.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactLoading from 'react-loading';
-export default function Loading({ type, color }) {
-  return (
-    <ReactLoading type={type} color={color} height={'20%'} width={'20%'} />
-  )
-}
--- src/Hooks/useQueryCategory.jsx
+++
@@ -1,7 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-
-
-export default function useQueryCategory(key,fn) {
-  
-    return useQuery({queryKey:[key],queryFn:fn,select:(data)=>data})
-}
